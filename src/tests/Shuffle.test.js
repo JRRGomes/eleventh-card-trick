b6d9f6e21eb476419f4d7b06a38fa302e813eb27
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-08 17:22:45 -0300

    style: Changed the text from the it test to match the context.

--- src/tests/Shuffle.test.js
+++ src/tests/Shuffle.test.js
@@ -2,7 +2,7 @@ import CreateDeck from "../components/CreateDeck";
 import Shuffle from "../components/Shuffle";
 
 describe("Shuffle", () => {
-  it("should return a array of objects with 52 cards/suits", () => {
+  it("should return a array of objects with 52 cards/suits shuffled", () => {
     const deck = CreateDeck()
     const expected = [{"card": "A","suit": "H",}, {"card": "2","suit": "C",},{"card": "3","suit": "S",}]
     expect(Shuffle(deck)).toEqual(expect.arrayContaining(expected));
