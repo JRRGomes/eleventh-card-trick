5341cb92eb7eee95915b8196db7838db2a1ff072
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-10 10:58:05 -0300

    feat: Created buttons for each row and styles for buttons added as well.

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { DECK } from '../util/createDeck';
-import shuffle from '../util/shuffle';
 import DeckRows from './DeckRows';
 
 import '../styles/elements/_deck-container.css';
 import '../styles/elements/_deck.css';
 
 const Deck = () => {
-  const shuffleDeck = shuffle(DECK);
-  const twentyOneCards = shuffleDeck.slice(0,21);
-
-  const firstRow = [...twentyOneCards].splice(0,7);
-  const secondRow = [...twentyOneCards].splice(7,7);
-  const thirdRow = [...twentyOneCards].splice(14,7);
-
-  console.log(twentyOneCards)
-  console.log(firstRow)
-  console.log(secondRow)
-  console.log(thirdRow)
-
   return (
     <DeckRows />
   )
--- src/components/DeckRows.js
+++ src/components/DeckRows.js
@@ -2,28 +2,36 @@ import { DECK } from '../util/createDeck';
 import DeckCard from './DeckCard';
 import shuffle from '../util/shuffle';
 
+import "../styles/elements/_button.css";
 
 const DeckRows = () => {
   const shuffleDeck = shuffle(DECK);
   const twentyOneCards = shuffleDeck.slice(0,21);
-
+  
   const firstRow = [...twentyOneCards].splice(0,7);
   const secondRow = [...twentyOneCards].splice(7,7);
   const thirdRow = [...twentyOneCards].splice(14,7);
+  
+  const magicTrick = () => {
+    console.log('magic!')
+  }
 
   return (
     <>
     <div className='deck-container'> 
+      <button onClick={magicTrick} className='button'>My card is here</button>
       {firstRow.map((cardObject) => (
         <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
       ))}
     </div>
-    <div className='deck-container'> 
+    <div className='deck-container'>
+      <button className='button'>My card is here</button>
       {secondRow.map((cardObject) => (
         <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
       ))}
     </div>
-    <div className='deck-container'> 
+    <div className='deck-container'>
+      <button className='button'>My card is here</button> 
       {thirdRow.map((cardObject) => (
         <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
       ))}
