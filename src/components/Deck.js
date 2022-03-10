fd8c31c590f3acac3083c344b3b2f179dd26c070
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-10 09:25:00 -0300

    feat: Created DeckRows component to generate the 3 diferent rows of 7 cards

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -5,17 +5,23 @@ import DeckCard from './DeckCard';
 
 import '../styles/elements/_deck-container.css';
 import '../styles/elements/_deck.css';
+import DeckRows from './DeckRows';
 
 const Deck = () => {
   const shuffleDeck = shuffle(DECK);
   const twentyOneCards = shuffleDeck.slice(0,21);
 
+  const firstRow = [...twentyOneCards].splice(0,7);
+  const secondRow = [...twentyOneCards].splice(7,7);
+  const thirdRow = [...twentyOneCards].splice(14,7);
+
+  console.log(twentyOneCards)
+  console.log(firstRow)
+  console.log(secondRow)
+  console.log(thirdRow)
+
   return (
-    <div className='deck-container'> 
-      {twentyOneCards.map((cardObject) => (
-        <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
-      ))}
-    </div>
+    <DeckRows />
   )
 }
 
---
+++ src/components/DeckRows.js
@@ -0,0 +1,35 @@
+import { DECK } from '../util/createDeck';
+import DeckCard from './DeckCard';
+import shuffle from '../util/shuffle';
+
+
+const DeckRows = () => {
+  const shuffleDeck = shuffle(DECK);
+  const twentyOneCards = shuffleDeck.slice(0,21);
+
+  const firstRow = [...twentyOneCards].splice(0,7);
+  const secondRow = [...twentyOneCards].splice(7,7);
+  const thirdRow = [...twentyOneCards].splice(14,7);
+
+  return (
+    <>
+    <div className='deck-container'> 
+      {firstRow.map((cardObject) => (
+        <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
+      ))}
+    </div>
+    <div className='deck-container'> 
+      {secondRow.map((cardObject) => (
+        <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
+      ))}
+    </div>
+    <div className='deck-container'> 
+      {thirdRow.map((cardObject) => (
+        <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default DeckRows
