4a7b06659d4f7e7b538cd679ac5391b7d0224d3d
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-10 15:31:41 -0300

    style: Removed tags once Im passing only one div

--- src/components/DeckRow.js
+++ src/components/DeckRow.js
@@ -3,13 +3,11 @@ import DeckCard from './DeckCard';
 const DeckRow = ({row}) => {
 
   return (
-    <>
       <div className='deck-container'>
         {row.map((cardObject) => (
           <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
         ))}
       </div>
-    </>
   )
 }
 
