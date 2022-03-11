0909c195f478393571cd465a9000bb83ab28fb5e
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-11 17:45:27 -0300

    feat: Created putRowOnCenter function to reorder my Trickdeck following the magic instructions. Created a hook to handle the user click

--- src/components/DeckRow.js
+++ src/components/DeckRow.js
@@ -2,10 +2,11 @@ import DeckCard from './DeckCard';
 
 import '../styles/elements/_button.css';
 
-const DeckRow = ({row}) => {
+const DeckRow = ({row, handleButtonClick}) => {
+
   return (
       <div className='deck-container'>
-        <button className='button'>my card is here</button>
+        <button onClick={handleButtonClick} className='button'>my card is here</button>
         {row.map((cardObject) => (
           <DeckCard key={`${cardObject.suit}-${cardObject.card}`} {...cardObject} />
         ))}
